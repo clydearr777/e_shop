15056dce5d4cc88a5231692d188194103070281f
Max <clyde.arrow@gmail.com>
2023-09-19 00:47:13 +0500

    дз-2 и тесты

--- src/item.py
+++ src/item.py
@@ -1,5 +1,6 @@
 import csv
 
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -18,29 +19,53 @@ class Item:
         self.quantity = quantity
         self.all.append(self.__name)
 
+    @property
+    def name(self):
+        """homework-2 Геттер для приватного параметра __name"""
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        """ Homework-2 метод проверяет количество символов в параметре __name и обрезает в случае если их >10"""
+        if len(name) > 10:
+            self.__name = name[:10]
+        else:
+            self.__name = name
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        """
+        Homework-2
+        Инициализирует экземпляры класса данными из файла
+        """
+        Item.all = []
+        path = f'../{path}'
+        with open(path, newline='') as f:
+            file = csv.DictReader(f)
+            for info in file:
+                Item(info['name'], info['price'], info['quantity'])
+
+    @staticmethod
+    def string_to_number(number):
+        """ HOMEWORK-2  Возвращает число (int) вместо строки"""
+        return int(float(number))
+
+
     def __repr__(self):
         """Метод возвращение в виде 'Название класса(имя, цена, кол-во)"""
-        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+        return f"{self.__class__.__name__}('{self.__name}', {self.price}, {self.quantity})"
 
     def __str__(self):
-        return self.name
+        return self.__name
+
+
+
 
     def __add__(self, other):
         """метод реализует суммирование экземплров класса по количеству в магазине"""
         return self.quantity + other.quantity
 
-    def get_name(self):
-        return self.__name
-
-    def set_name(self, name):
-        """метод проверяет количество символов в названии и обрезает в случае если символов >10"""
-        if len(name) > 0:
-            name = name[:10]
-        self.__name = name
-
-    name = property(get_name, set_name)
-
-    def calculate_total_price(self) -> float:
+    def calculate_total_price(self):
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
         :return: Общая стоимость товара.
@@ -53,20 +78,3 @@ class Item:
         """
         self.price *= self.pay_rate
         return self.price
-
-    @classmethod
-    def instantiate_from_csv(cls, path):
-        """
-        Инициализирует экземпляры класса данными из файла
-        """
-        Item.all =[]
-        # path = f'../{path}'
-        with open(path, newline='') as f:
-            file = csv.DictReader(f)
-            for info in file:
-                Item(info['name'],info['price'],info['quantity'])
-
-    def string_to_number(number):
-        """Возвращает число (int) вместо строки"""
-        return int(float(number))
-
