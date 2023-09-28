49cac8a11c9406abfc3c311ac68139b4e66499fb
Max <clyde.arrow@gmail.com>
2023-09-28 10:50:24 +0500

    дз-4 начато

--- src/phone.py
+++ src/phone.py
@@ -6,25 +6,16 @@ class Phone(Item):
     класс-наследник от класса ITEM
     параметр number_of_sim - количество сим-карт
     """
+
+    # number_of_sim = 1
     def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
-        try:
-            if number_of_sim <= 0 or number_of_sim is not int:
-                raise ValueError()
-            else:
-                self.number_of_sim = number_of_sim
-        except ValueError:
-            print("Количество физических SIM-карт должно быть целым числом больше нуля.")
         super().__init__(name, price, quantity)
+        self.number_of_sim = number_of_sim
+        number_of_sim: int = 1
 
     # @number_of_sim.setter
     # def number_of_sim(self, sim):
     #     pass
 
-
-
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
-
-
-
-
