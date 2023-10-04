98e6624b6881840e271290b7bb10e6a32814f11b
Max <clyde.arrow@gmail.com>
2023-10-04 21:13:06 +0500

    дз-5 исправлено

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,6 +4,7 @@ from src.item import Item
 class Mixin:
     """ Класс-миксин для добавления метода переключения языка
         дополнительного параметра - language, по умолчанию -EN + сеттер """
+    __language = 'EN'
 
     def __init__(self, name, price, quantity):
         super().__init__(name, price, quantity)
@@ -14,7 +15,7 @@ class Mixin:
         Геттер для параметра language, по умолчанию - EN, далее переключение языка
         возможно только через метод change_lang
         """
-        self.__language = 'EN'
+
         return self.__language
 
     @language.setter
@@ -23,16 +24,18 @@ class Mixin:
         параметр не может принимать значения кроме RU и EN"""
         if value != 'EN' or value != 'RU':
             raise AttributeError("property 'language' of 'Keyboard' object has no setter")
+        else:
+            self.__language = value
 
     def change_lang(self) -> None:
         """
         метод, для переключения языка (иначе никак)
         """
-        if self.language == 'EN':
-            self.language = 'RU'
+        if self.__language == 'EN':
+            self.__language = 'RU'
             print('переключили На русский')
         else:
-            self.language = 'EN'
+            self.__language = 'EN'
             print('переключили На англ')
 
 
